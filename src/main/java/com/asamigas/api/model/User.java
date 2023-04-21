78edf5bf81d59b610f14c6b75f426eef6c9bc6b4
José João Dantas <jose_dantas_80@hotmail.com>
2023-04-21 17:51:04 +0100

    TODO: Review User entity

--- src/main/java/com/asamigas/api/model/User.java
+++ src/main/java/com/asamigas/api/model/User.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @Getter
 @NoArgsConstructor
 public class User {
+    //TODO REVIEW IF THERE'S MORE FIELDS THAT ARE NEEDED
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
